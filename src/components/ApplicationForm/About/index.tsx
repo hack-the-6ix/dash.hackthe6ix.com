40459b626df997d9b3bfbed54f7ca6865039cdcc
Gordon Lin <contact@gordonlin.ca>
2023-07-21 22:04:06 -0400

    Added field styling

--- src/components/ApplicationForm/About/index.tsx
+++ src/components/ApplicationForm/About/index.tsx
@@ -1,4 +1,11 @@
-import {Checkbox, Dropdown, Input, SearchDropdown, Textarea, Typography} from '@ht6/react-ui';
+import {
+  Checkbox,
+  Dropdown,
+  Input,
+  SearchDropdown,
+  Textarea,
+  Typography,
+} from '@ht6/react-ui';
 import { useEffect } from 'react';
 import * as yup from 'yup';
 
@@ -7,6 +14,7 @@ import ApplicationFormSection from '../../ApplicationFormSection';
 import { SectionProps, useFormikHelpers } from '../helpers';
 
 import sharedStyles from '../ApplicationForm.module.scss';
+import styles from './About.module.scss';
 
 export const initialValues = {
   firstName: '',
@@ -28,8 +36,8 @@ export const initialValues = {
     firstName: '',
     lastName: '',
     phoneNumber: '',
-    relationship: ''
-  }
+    relationship: '',
+  },
 };
 
 export const validate = yup.lazy((values) => {
@@ -42,7 +50,12 @@ export const validate = yup.lazy((values) => {
       .required("Email can't be blank"),
     phone: yup.string().required("Phone Number can't be blank"),
     canEmail: yup.boolean(),
-    age: yup.number().typeError("Age must be a number").required("Age can't be blank").positive("Age must be positive").integer("Age must be an integer"),
+    age: yup
+      .number()
+      .typeError('Age must be a number')
+      .required("Age can't be blank")
+      .positive('Age must be positive')
+      .integer('Age must be an integer'),
     gender: yup.string().required("Gender can't be blank"),
     pronouns: yup.string().required("Pronouns can't be blank"),
     ethnicity: yup.string().required("Ethnicity can't be blank"),
@@ -51,15 +64,23 @@ export const validate = yup.lazy((values) => {
     province: yup.string().required("Province/State can't be blank"),
     country: yup.string().required("Country can't be blank"),
     emergencyContact: yup.object({
-        firstName: yup.string().required("Emergency contact first name can't be blank"),
-        lastName: yup.string().required("Emergency contact last name can't be blank"),
-        phoneNumber: yup.string().required("Emergency contact phone number can't be blank"),
-        relationship: yup.string().required("Emergency contact relationship can't be blank")
-    })
+      firstName: yup
+        .string()
+        .required("Emergency contact first name can't be blank"),
+      lastName: yup
+        .string()
+        .required("Emergency contact last name can't be blank"),
+      phoneNumber: yup
+        .string()
+        .required("Emergency contact phone number can't be blank"),
+      relationship: yup
+        .string()
+        .required("Emergency contact relationship can't be blank"),
+    }),
   } as any;
 
   return yup.object({
-    ...schema
+    ...schema,
   });
 }) as any;
 
@@ -75,6 +96,7 @@ function About(props: SectionProps<typeof initialValues>) {
           label: 'First Name',
           required: true,
         })}
+        className={styles.field}
         placeholder='Enter first name'
         disabled
       />
@@ -84,6 +106,7 @@ function About(props: SectionProps<typeof initialValues>) {
           label: 'Last Name',
           required: true,
         })}
+        className={styles.field}
         placeholder='Enter last name'
         disabled
       />
@@ -93,6 +116,7 @@ function About(props: SectionProps<typeof initialValues>) {
           label: 'Email',
           required: true,
         })}
+        className={styles.field}
         placeholder='name@gmail.com'
         type='email'
         disabled
@@ -103,6 +127,7 @@ function About(props: SectionProps<typeof initialValues>) {
           label: 'Phone Number',
           required: true,
         })}
+        className={styles.field}
         placeholder='1234567890'
         type='phone'
       />
@@ -119,10 +144,11 @@ function About(props: SectionProps<typeof initialValues>) {
       />
       <Input
         {...applyFieldProps({
-            name: 'age',
-            label: 'Age',
-            required: true,
+          name: 'age',
+          label: 'Age',
+          required: true,
         })}
+        className={styles.field}
         placeholder='Your age'
       />
       <Dropdown
@@ -161,140 +187,147 @@ function About(props: SectionProps<typeof initialValues>) {
           label,
         }))}
       />
-        <Dropdown
-            {...applyFieldProps({
-                name: 'size',
-                label: 'Shirt Size',
-                omitOutline: true,
-                required: true,
-            })}
-            options={enums.shirt.map((label) => ({
-                value: label,
-                label,
-            }))}
-        />
-        <Dropdown
-            {...applyFieldProps({
-                name: 'dietaryRestrictions',
-                label: 'Dietary Restrictions',
-                omitOutline: true
-            })}
-            options={enums.dietaryRestrictions.map((label) => ({
-                value: label,
-                label,
-            }))}
-        />
-        <div className={sharedStyles['field--full-width']}>
-            <Typography textColor='primary-700' textType='heading3' as='h2'>
-                Your Location
-            </Typography>
-            <Typography textColor='copy-dark' textType='paragraph1' as='p'>
-                If you are travelling far from the Toronto area, you may be eligible to apply for a travel reimbursement. We'll reach out with more details if that's the case.
-            </Typography>
-        </div>
-        <Input
-            {...applyFieldProps({
-                name: 'city',
-                label: 'City',
-                required: true,
-            })}
-            placeholder='Enter city name'
-        />
+      <Dropdown
+        {...applyFieldProps({
+          name: 'size',
+          label: 'Shirt Size',
+          omitOutline: true,
+          required: true,
+        })}
+        options={enums.shirt.map((label) => ({
+          value: label,
+          label,
+        }))}
+      />
+      <Dropdown
+        {...applyFieldProps({
+          name: 'dietaryRestrictions',
+          label: 'Dietary Restrictions',
+          omitOutline: true,
+        })}
+        options={enums.dietaryRestrictions.map((label) => ({
+          value: label,
+          label,
+        }))}
+      />
+      <div className={sharedStyles['field--full-width']}>
+        <Typography textColor='primary-700' textType='heading3' as='h2'>
+          Your Location
+        </Typography>
+        <Typography textColor='copy-dark' textType='paragraph1' as='p'>
+          If you are travelling far from the Toronto area, you may be eligible
+          to apply for a travel reimbursement. We'll reach out with more details
+          if that's the case.
+        </Typography>
+      </div>
+      <Input
+        {...applyFieldProps({
+          name: 'city',
+          label: 'City',
+          required: true,
+        })}
+        className={styles.field}
+        placeholder='Enter city name'
+      />
 
-        <SearchDropdown
-            {...applyFieldProps({
-                name: 'province',
-                label: 'Province / State',
-                required: true,
-                omitOutline: true,
-            })}
-            placeholder='Select an option'
-            options={enums.province.map((label) => ({
-                value: label,
-                label,
-            }))}
-            onChange={(e) => {
-                props.setFieldValue('province', e.currentTarget.value);
-            }}
-        />
-        <Dropdown
-            {...applyFieldProps({
-                name: 'country',
-                label: 'Country',
-                required: true,
-                omitOutline: true,
-            })}
-            disabled={props.readonly}
-            options={enums.countries.map((label) => ({
-                value: label,
-                label,
-            }))}
-        />
-        <div className={sharedStyles['field--full-width']}>
-            <Typography textColor='primary-700' textType='heading3' as='h2'>
-                Emergency Contact
-            </Typography>
-            <Typography textColor='copy-dark' textType='paragraph1' as='p'>
-                Your safety is our priority. In the case of an emergency, the person below will be contacted.
-                We respect your privacy and guarantee that this information will only be accessed by authorized personnel on an as-needed basis.
-            </Typography>
-        </div>
-        <Input
-            {...applyFieldProps({
-                name: 'emergencyContact.firstName',
-                label: 'First Name',
-                required: true,
-            })}
-            placeholder='Enter emergency contact first name'
-        />
-        <Input
-            {...applyFieldProps({
-                name: 'emergencyContact.lastName',
-                label: 'Last Name',
-                required: true,
-            })}
-            placeholder='Enter emergency contact last name'
-        />
-        <Input
-            {...applyFieldProps({
-                name: 'emergencyContact.phoneNumber',
-                label: 'Phone Number',
-                required: true,
-            })}
-            placeholder='1234567890'
-            type='phone'
-        />
-        <Dropdown
-            {...applyFieldProps({
-                name: 'emergencyContact.relationship',
-                label: 'Relationship',
-                required: true,
-                omitOutline: true,
-            })}
-            disabled={props.readonly}
-            options={enums.emergencyContactRelationship.map((label) => ({
-                value: label,
-                label,
-            }))}
-        />
-        <div className={sharedStyles['field--full-width']}>
-            <Typography textColor='primary-700' textType='heading3' as='h2'>
-                Additional Information
-            </Typography>
-        </div>
-        <Textarea
-            {...applyFieldProps({
-                name: 'healthWarnings',
-                label:
-                    'Please list all additional allergies/dietary restrictions or any other information we may need to accommodate you.',
-                omitOutline: true,
-                isNextRow: true,
-                isFullWidth: true
-            })}
-            limit={200}
-            rows={3}
-        />
-
+      <SearchDropdown
+        {...applyFieldProps({
+          name: 'province',
+          label: 'Province / State',
+          required: true,
+          omitOutline: true,
+        })}
+        placeholder='Select an option'
+        options={enums.province.map((label) => ({
+          value: label,
+          label,
+        }))}
+        onChange={(e) => {
+          props.setFieldValue('province', e.currentTarget.value);
+        }}
+      />
+      <Dropdown
+        {...applyFieldProps({
+          name: 'country',
+          label: 'Country',
+          required: true,
+          omitOutline: true,
+        })}
+        disabled={props.readonly}
+        options={enums.countries.map((label) => ({
+          value: label,
+          label,
+        }))}
+      />
+      <div className={sharedStyles['field--full-width']}>
+        <Typography textColor='primary-700' textType='heading3' as='h2'>
+          Emergency Contact
+        </Typography>
+        <Typography textColor='copy-dark' textType='paragraph1' as='p'>
+          Your safety is our priority. In the case of an emergency, the person
+          below will be contacted. We respect your privacy and guarantee that
+          this information will only be accessed by authorized personnel on an
+          as-needed basis.
+        </Typography>
+      </div>
+      <Input
+        {...applyFieldProps({
+          name: 'emergencyContact.firstName',
+          label: 'First Name',
+          required: true,
+        })}
+        className={styles.field}
+        placeholder='Enter emergency contact first name'
+      />
+      <Input
+        {...applyFieldProps({
+          name: 'emergencyContact.lastName',
+          label: 'Last Name',
+          required: true,
+        })}
+        className={styles.field}
+        placeholder='Enter emergency contact last name'
+      />
+      <Input
+        {...applyFieldProps({
+          name: 'emergencyContact.phoneNumber',
+          label: 'Phone Number',
+          required: true,
+        })}
+        className={styles.field}
+        placeholder='1234567890'
+        type='phone'
+      />
+      <Dropdown
+        {...applyFieldProps({
+          name: 'emergencyContact.relationship',
+          label: 'Relationship',
+          required: true,
+          omitOutline: true,
+        })}
+        disabled={props.readonly}
+        options={enums.emergencyContactRelationship.map((label) => ({
+          value: label,
+          label,
+        }))}
+      />
+      <div className={sharedStyles['field--full-width']}>
+        <Typography textColor='primary-700' textType='heading3' as='h2'>
+          Additional Information
+        </Typography>
+      </div>
+      <Textarea
+        {...applyFieldProps({
+          name: 'healthWarnings',
+          label:
+            'Please list all additional allergies/dietary restrictions or any other information we may need to accommodate you.',
+          omitOutline: true,
+          isNextRow: true,
+          isFullWidth: true,
+        })}
+        limit={200}
+        rows={3}
+      />
     </ApplicationFormSection>
   );
 }
